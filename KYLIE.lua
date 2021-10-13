ad75cb80417bad68318c58db1d865dd6a2122a33
KYLIE-TEAM <84586775+KYLIE-TEAM@users.noreply.github.com>
2021-10-14 02:29:55 +0300

    Add files via upload

--- KYLIE.lua
+++ KYLIE.lua
@@ -383,11 +383,18 @@ local url , res = https.request("https://api.telegram.org/bot"..token.."/getchat
 data = json:decode(url)
 if res ~= 200 or data.result.status == "left" or data.result.status == "kicked" then
 var = false
-end
-end
+local Text = database:get(bot_id..'text:ch:user') or '*✬︙عذࢪا عليڪ الاشتࢪاڪ بالقناه*'
+local Usext = database:get(bot_id..'add:ch:username'):gsub('@',"")
+keyboard = {} 
+keyboard.inline_keyboard = {{{text ="اضغط هنا للاشتࢪاڪ عمࢪي",url="t.me/"..Usext}},}
+local msg_id = msg.id_/2097152/0.5
+https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+elseif data.ok then
 return var
 end
-function dl_cb(a,d)
+else
+return var
+end
 end
 function getChatId(id)
 local chat = {}
