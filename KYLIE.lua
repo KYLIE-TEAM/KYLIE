aac2b7a813056ab89de1cabe3f8bb489a4909438
KYLIE-TEAM <84586775+KYLIE-TEAM@users.noreply.github.com>
2021-10-14 03:42:33 +0300

    Add files via upload

--- KYLIE.lua
+++ KYLIE.lua
@@ -2624,8 +2624,7 @@ keyboard.inline_keyboard = {
 {{text = '✬ sᴏᴜʀᴄʀ ᴋʏʟɪᴇ',url="t.me/PPPPD"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-return false
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/PPPPD&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 --------------------------------------------------------------------------------------------------------------
 if Chat_Type == 'GroupBot' and ChekAdd(msg.chat_id_) == true then
